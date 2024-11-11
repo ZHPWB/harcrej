a7aebfba6b7f5a3abd52f4905542bc7183fd7e6e
Jan Andrzejczyk <33021127+jka2498@users.noreply.github.com>
2024-11-11 22:31:48 +0000

    initial commit

---
+++ src/main/java/com/zhpwb/harcrej/HarcrejApplication.java
@@ -0,0 +1,13 @@
+package com.zhpwb.harcrej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarcrejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarcrejApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhpwb/harcrej/HarcrejApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhpwb.harcrej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarcrejApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
